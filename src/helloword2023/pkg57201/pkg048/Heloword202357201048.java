762755076eda64347e88ed6c89c360de9baa9c9c
lenovo <lenovo@DESKTOP-T41SDQR>
2024-03-14 19:20:01 -0800

    add helo word 2023.57201.048

---
+++ src/helloword2023/pkg57201/pkg048/Heloword202357201048.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package helloword2023.pkg57201.pkg048;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Heloword202357201048 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("helo word 2023.57201.048");
+    }
+    
+}
